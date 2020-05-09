83f250dd92ea2c0eb6050f3b8a46b14e2be7c2a8
Murray Lee <murraylee10@gmail.com>
2020-05-09 15:13:07 -0500

    separated js functions

--- hedera/index.js
+++ hedera/index.js
@@ -1,8 +1,7 @@
 const { Client, FileCreateTransaction, Ed25519PrivateKey, Hbar, FileContentsQuery, FileId } = require("@hashgraph/sdk");
 require("dotenv").config();
 
-async function main() {
-  
+async function makeFile() {
   const operatorAccount = process.env.OPERATOR_ID;
   const operatorPrivateKey = Ed25519PrivateKey.fromString(process.env.OPERATOR_KEY);
   const operatorPublicKey = operatorPrivateKey.publicKey;
@@ -26,15 +25,37 @@ async function main() {
     const receipt = await transactionId.getReceipt(client); 
     const fileId = receipt.getFileId(); 
     console.log("new file id = ", fileId);
-
-
-    // get file contents
-    const resp = await new FileContentsQuery()
-        // .setFileId(FileId.ADDRESS_BOOK)
-        .setFileId(fileId)
-        .execute(client);
-
-    console.log(`file contents: ${new TextDecoder().decode(resp)}`)
+    return fileId;
 }
 
-main();
+async function getFile(fileId)
+{
+  const operatorAccount = process.env.OPERATOR_ID;
+  const operatorPrivateKey = Ed25519PrivateKey.fromString(process.env.OPERATOR_KEY);
+  const operatorPublicKey = operatorPrivateKey.publicKey;
+
+  if (operatorPrivateKey == null || operatorAccount == null) {
+    throw new Error(
+      "environment variables OPERATOR_KEY and OPERATOR_ID must be present"
+    );
+  }
+
+  const client = Client.forTestnet();
+  client.setOperator(operatorAccount, operatorPrivateKey);
+  // get file contents
+  const resp = await new FileContentsQuery()
+  // .setFileId(FileId.ADDRESS_BOOK)
+  .setFileId(fileId)
+  .execute(client);
+
+  console.log(`file contents: ${new TextDecoder().decode(resp)}`) 
+}
+
+async function main()
+{
+  let id = await makeFile();
+  console.log(id);
+  getFile(id);
+}
+
+main()
